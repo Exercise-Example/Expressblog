e6cb20799a414b97072758d029aa69eec691cdf5
gh286991 <gh286991@gmail.com>
2016-10-20 23:23:39 +0800

    套用 BOOTSTRAP 測試 PAGE
    套用 BOOTSTRAP 測試 PAGE

--- blog-system/routes/index.js
+++ blog-system/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Blog System' });
 });
 
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'Blog System' });
+});
 module.exports = router;
